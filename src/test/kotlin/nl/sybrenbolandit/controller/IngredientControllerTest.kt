506f045370a6f3ba8a1f4f5b260d1e6f96f97132
sybrenbolandit <sybrenbolandit@gmail.com>
2019-03-17 16:40:35 +0100

    Default port for automatic heroku routing

--- src/test/kotlin/nl/sybrenbolandit/controller/IngredientControllerTest.kt
+++ src/test/kotlin/nl/sybrenbolandit/controller/IngredientControllerTest.kt
@@ -3,7 +3,7 @@ package nl.sybrenbolandit.controller
 import io.micronaut.context.ApplicationContext
 import io.micronaut.http.client.HttpClient
 import io.micronaut.runtime.server.EmbeddedServer
-import junit.framework.Assert.assertEquals
+import junit.framework.TestCase.assertEquals
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
