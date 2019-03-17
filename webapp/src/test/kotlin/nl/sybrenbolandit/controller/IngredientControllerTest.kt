05879cc3b4ce270ac52cd61cd414cd6548fa994a
sybrenbolandit <sybrenbolandit@gmail.com>
2019-03-17 19:12:20 +0100

    Structured in multiple modules

--- webapp/src/test/kotlin/nl/sybrenbolandit/controller/IngredientControllerTest.kt
+++ webapp/src/test/kotlin/nl/sybrenbolandit/controller/IngredientControllerTest.kt
@@ -10,11 +10,11 @@ import org.jetbrains.spek.api.dsl.it
 
 object IngredientControllerTest: Spek({
     describe("HelloController Suite") {
-        var embeddedServer : EmbeddedServer = ApplicationContext.run(EmbeddedServer::class.java)
-        var client : HttpClient = HttpClient.create(embeddedServer.url)
+        val embeddedServer : EmbeddedServer = ApplicationContext.run(EmbeddedServer::class.java)
+        val client : HttpClient = HttpClient.create(embeddedServer.url)
 
         it("test /hello responds Hello World") {
-            var rsp : String = client.toBlocking().retrieve("/hello")
+            val rsp : String = client.toBlocking().retrieve("/hello")
             assertEquals(rsp, "Hello World")
         }
 
