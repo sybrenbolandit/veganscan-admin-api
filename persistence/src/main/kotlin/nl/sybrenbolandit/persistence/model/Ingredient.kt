aca42f025bf519bcf94feebad3756bfcec92d05d
sybrenbolandit <sybrenbolandit@gmail.com>
2019-03-22 19:34:09 +0100

    Changed ingredient model

--- persistence/src/main/kotlin/nl/sybrenbolandit/persistence/model/Ingredient.kt
+++ persistence/src/main/kotlin/nl/sybrenbolandit/persistence/model/Ingredient.kt
@@ -5,7 +5,7 @@ import org.bson.codecs.pojo.annotations.BsonProperty
 
 data class Ingredient @BsonCreator constructor(
         @param:BsonProperty("name") val name: String,
-        @param:BsonProperty("synonyms") val synonyms: String?,
+        @param:BsonProperty("synonyms") val synonyms: List<String>,
         @param:BsonProperty("type") val type: IngredientType,
         @param:BsonProperty("description") val description: String?
 )
