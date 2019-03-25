17ec558bcaac72cb1b79c4df1c183977a76ba5c2
sybrenbolandit <sybrenbolandit@gmail.com>
2019-03-25 19:06:49 +0100

    Added exception handling

--- webapp/src/main/kotlin/nl/sybrenbolandit/webapp/controller/IngredientController.kt
+++ webapp/src/main/kotlin/nl/sybrenbolandit/webapp/controller/IngredientController.kt
@@ -1,12 +1,11 @@
 package nl.sybrenbolandit.webapp.controller
 
-import io.micronaut.http.MediaType
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Consumes
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
-import io.micronaut.http.annotation.Produces
+import io.micronaut.http.annotation.Status
 import io.micronaut.validation.Validated
 import io.reactivex.Single
 import nl.sybrenbolandit.persistence.model.Ingredient
@@ -18,14 +17,12 @@ import javax.validation.Valid
 class IngredientController(val ingredientRepository: IngredientRepository) {
 
     @Post("/")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
+    @Status(HttpStatus.CREATED)
     fun createIngredient(@Body @Valid ingredient: Ingredient): Single<Ingredient> {
         return ingredientRepository.createIngredient(ingredient)
     }
 
     @Get("/")
-    @Produces(MediaType.APPLICATION_JSON)
     fun ingredientList(): Single<List<Ingredient>> {
         return ingredientRepository.fetchIngredientList()
     }
--- webapp/src/main/kotlin/nl/sybrenbolandit/webapp/controller/ProductController.kt
+++ webapp/src/main/kotlin/nl/sybrenbolandit/webapp/controller/ProductController.kt
@@ -1,33 +1,42 @@
 package nl.sybrenbolandit.webapp.controller
 
-import io.micronaut.http.MediaType
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Consumes
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Error
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
-import io.micronaut.http.annotation.Produces
+import io.micronaut.http.annotation.Status
+import io.micronaut.http.hateos.JsonError
 import io.micronaut.validation.Validated
 import io.reactivex.Maybe
 import io.reactivex.Single
 import nl.sybrenbolandit.persistence.model.Product
 import nl.sybrenbolandit.persistence.repository.ProductRepository
 import javax.validation.Valid
+import javax.validation.constraints.NotBlank
 
 @Validated
 @Controller("/products")
 class ProductController(val productRepository: ProductRepository) {
 
     @Post("/")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
+    @Status(HttpStatus.CREATED)
     fun createProduct(@Body @Valid product: Product): Single<Product> {
         return productRepository.createProduct(product)
     }
 
     @Get("/{barcode}")
-    @Produces(MediaType.APPLICATION_JSON)
-    fun findProduct(barcode: String): Maybe<Product> {
+    fun findProduct(@NotBlank barcode: String): Maybe<Product> {
         return productRepository.fetchProduct(barcode.toInt())
     }
+
+    @Error(global = true)
+    fun error(request: HttpRequest<Any>, e: Throwable): HttpResponse<JsonError> =
+            when (e) {
+                is NumberFormatException -> HttpResponse.notFound()
+                else -> HttpResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+            }
 }
