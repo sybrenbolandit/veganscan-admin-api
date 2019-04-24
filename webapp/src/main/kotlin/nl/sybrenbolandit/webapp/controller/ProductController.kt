d78f786049540a990eef1f64737ea359a434b4b3
sybrenbolandit <sybrenbolandit@gmail.com>
2019-04-24 21:29:41 +0100

    Run script runs dev mode

--- webapp/src/main/kotlin/nl/sybrenbolandit/webapp/controller/ProductController.kt
+++ webapp/src/main/kotlin/nl/sybrenbolandit/webapp/controller/ProductController.kt
@@ -1,15 +1,11 @@
 package nl.sybrenbolandit.webapp.controller
 
-import io.micronaut.http.HttpRequest
-import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Error
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.Status
-import io.micronaut.http.hateos.JsonError
 import io.micronaut.validation.Validated
 import io.reactivex.Maybe
 import io.reactivex.Single
@@ -32,11 +28,4 @@ class ProductController(val productRepository: ProductRepository) {
     fun findProduct(@NotBlank barcode: String): Maybe<Product> {
         return productRepository.fetchProduct(barcode)
     }
-
-    @Error(global = true)
-    fun error(request: HttpRequest<Any>, e: Throwable): HttpResponse<JsonError> =
-            when (e) {
-                is NumberFormatException -> HttpResponse.notFound()
-                else -> HttpResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
-            }
 }
