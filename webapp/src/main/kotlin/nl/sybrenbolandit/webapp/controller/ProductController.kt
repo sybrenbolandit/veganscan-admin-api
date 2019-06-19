0709e6aaf5b5ce5384b7fefde02f11576bae52af
sybrenbolandit <sybrenbolandit@gmail.com>
2019-06-19 19:32:27 +0200

    Removed error handling

--- webapp/src/main/kotlin/nl/sybrenbolandit/webapp/controller/ProductController.kt
+++ webapp/src/main/kotlin/nl/sybrenbolandit/webapp/controller/ProductController.kt
@@ -1,16 +1,11 @@
 package nl.sybrenbolandit.webapp.controller
 
-import com.mongodb.MongoWriteException
-import io.micronaut.http.HttpRequest
-import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Error
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.Status
-import io.micronaut.http.hateos.JsonError
 import io.micronaut.security.annotation.Secured
 import io.micronaut.validation.Validated
 import io.reactivex.Maybe
@@ -35,11 +30,4 @@ class ProductController(val productRepository: ProductRepository) {
     fun findProduct(@NotBlank barcode: String): Maybe<Product> {
         return productRepository.fetchProduct(barcode)
     }
-
-    @Error(global = true)
-    fun error(request: HttpRequest<Any>, e: Throwable): HttpResponse<JsonError> =
-            when (e) {
-                is MongoWriteException -> HttpResponse.badRequest(JsonError("Object with this key already exists"))
-                else -> HttpResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
-            }
 }
